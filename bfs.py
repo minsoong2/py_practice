73de7f26e18c0e730ad17318bc5d9e72ed80303d
minsoong2 <happyms817@gmail.com>
2023-07-25 16:23:50 +0900

    230725

---
+++ bfs.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+
+def deque_bfs(g, start):
+
+    pre_visit = []
+    need_visit = deque([start])
+
+    while need_visit:
+        node = need_visit[0]
+        del need_visit[0]
+
+        if node not in pre_visit:
+            pre_visit.append(node)
+            need_visit.extend(g[node])
+
+    return pre_visit
+
+
+graph = dict()
+
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+print(deque_bfs(graph, 'A'))
