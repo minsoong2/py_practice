00e527185b894dcb4e1719e759bc66c612365103
minsoong2 <happyms817@gmail.com>
2023-07-26 16:27:27 +0900

    230726

--- prac.py
+++ prac.py
@@ -1,5 +1,17 @@
-a, b, c = map(int, input().split())
+# import sys
+# a, b, c = map(int, sys.stdin.readline().split)
+# a = int(sys.stdin.readline())
+# a, b, c = map(int, input().split())
 
-c = int(input())
-# b = int(input())
+a = int(input())
+b = int(input())
 
+sum = 0
+for i in range(b):
+    c, d = map(int, input().split())
+    sum += c*d
+
+if a == sum:
+    print('Yes')
+else:
+    print('No')
