8cd9ca3fefedd6546bcbf06d5de615b2a4f0e222
minsoong2 <happyms817@gmail.com>
2023-07-28 00:01:59 +0900

    230728

--- 10811.py
+++ 10811.py
@@ -6,14 +6,21 @@
 # list -> int는 append x
 # print(i, end=' ') end='' 줄바꿈 제거
 # cnt = len(set(list)) * set -> 중복제거 *
-#
 
 # a = int(input())
 # b = int(input())
 
-a = int(input())
-sum = 0
+a, b = map(int, input().split())
+
+l = [0] * a
 for i in range(a):
-    b = map(int, input())
-    sum += b
-print(sum)
+    l[i] = i + 1
+
+arr = []
+for i in range(b):
+    c, d = map(int, input().split())
+    arr = l[c-1:d][::-1]
+    l[c-1:d] = arr
+
+for i in range(a):
+    print(l[i], end=' ')
--- 3052.py
+++ 3052.py
@@ -6,14 +6,16 @@
 # list -> int는 append x
 # print(i, end=' ') end='' 줄바꿈 제거
 # cnt = len(set(list)) * set -> 중복제거 *
-#
 
 # a = int(input())
 # b = int(input())
 
-a = int(input())
-sum = 0
-for i in range(a):
-    b = map(int, input())
-    sum += b
-print(sum)
+l = [0] * 10
+l_1 = [0] * 10
+
+for i in range(10):
+    l[i] = int(input())
+    l_1[i] = l[i] % 42
+
+cnt = len(set(l_1))
+print(cnt)
