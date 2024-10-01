c3acbb97b808f5805961dad3d4685f2672debfcb
minsoong2 <happyms817@gmail.com>
2024-10-01 20:48:58 +0900

    prac until 1

--- Greedy/until_1.py
+++ Greedy/until_1.py
@@ -4,14 +4,28 @@ num, div_num = map(int, sys.stdin.readline().split())
 cnt = 0
 
 while True:
-    if num % div_num == 0:
-        num = num / div_num
-        cnt += 1
-    else:
-        num = num - 1
-        cnt += 1
+    target = (num//div_num) * div_num
+    cnt += num - target
+    num = target
 
-    if num == 1:
+    if num < div_num:
         break
 
+    cnt += 1
+    num //= div_num
+
+cnt += num - 1
 print(cnt)
+
+# while True:
+#     if num % div_num == 0:
+#         num = num / div_num
+#         cnt += 1
+#     else:
+#         num = num - 1
+#         cnt += 1
+#
+#     if num == 1:
+#         break
+#
+# print(cnt)
