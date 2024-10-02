4815d88aa94136de60bcd8ada70d5dbd6fc7c6b9
minsoong2 <happyms817@gmail.com>
2024-10-02 21:05:28 +0900

    selection sort

--- Sorting/selection.py
+++ Sorting/selection.py
@@ -7,8 +7,6 @@ for i in range(len(arr)):
     for j in range(i + 1, len(arr)):
         if arr[min_idx] > arr[j]:
             min_idx = j
-    num = arr[i]
-    arr[i] = arr[min_idx]
-    arr[min_idx] = num
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
     print(arr)
