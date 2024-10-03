161b963647a5b197e2bf05683e4f2e718b245f13
minsoong2 <happyms817@gmail.com>
2024-10-03 22:11:51 +0900

    Udpate dp tips & fib

--- Dynamic_Programming/fib.py
+++ Dynamic_Programming/fib.py
@@ -1,6 +1,31 @@
-def fib(num):
-    if num == 1 or num == 2:
-        return 1
-    return fib(num - 1) + fib(num - 2)
+import time
+# def fib(num):
+#     if num == 1 or num == 2:
+#         return 1
+#     return fib(num - 1) + fib(num - 2)
+#
+# print(fib(10)) # 100 x
 
-print(fib(10))
+# Top-down fib
+# d = [0] * 1000
+# def fib(num):
+#     if num == 1 or num == 2:
+#         return 1
+#
+#     # Memorization
+#     if d[num] != 0:
+#         return d[num]
+#
+#     d[num] = fib(num - 1) + fib(num - 2)
+#     return d[num]
+# print(fib(999))
+
+# Bottom-up fib
+# d = [0] * 100
+# d[1], d[2] = 1, 1
+# n = 99
+#
+# for i in range(3, n + 1):
+#     d[i] = d[i - 1] + d[i - 2]
+#
+# print(d[n])
