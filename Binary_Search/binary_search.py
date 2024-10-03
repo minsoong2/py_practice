c3fa71e1cb44bf3a024de135539853352b97731f
minsoong2 <happyms817@gmail.com>
2024-10-03 18:07:36 +0900

    Update binary search loop ex

--- Binary_Search/binary_search.py
+++ Binary_Search/binary_search.py
@@ -1,6 +1,6 @@
 import sys
 
-def binary_search(arr, target,start,end):
+def binary_search_recursive(arr, target, start, end):
     if start > end:
         return None
 
@@ -9,14 +9,28 @@ def binary_search(arr, target,start,end):
     if arr[mid] == target:
         return mid
     elif arr[mid] < target:
-        return binary_search(arr, target, mid + 1, end)
+        return binary_search_recursive(arr, target, mid + 1, end)
     else:
-        return binary_search(arr, target, start, mid - 1)
+        return binary_search_recursive(arr, target, start, mid - 1)
+
+def binary_search_loop(arr, target, start, end):
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return None
 
 n, target = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
 
-result = binary_search(arr, target, 0, n - 1)
+result = binary_search_loop(arr, target, 0, n - 1)
 if result is None:
     print('No data')
 else:
