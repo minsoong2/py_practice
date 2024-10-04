49e6ebfec19d7e9be24fb96ea2300c290603ecaf
minsoong2 <happyms817@gmail.com>
2024-10-04 17:55:37 +0900

    Update ant_warrior

---
+++ Dynamic_Programming/ant_warrior.py
@@ -0,0 +1,13 @@
+import sys
+
+num = int(sys.stdin.readline())
+cnt_list = list(map(int, sys.stdin.readline().split()))
+
+dp_table = [0] * 100
+
+dp_table[0] = cnt_list[0]
+dp_table[1] = max(cnt_list[0], cnt_list[1])
+for i in range(2, len(cnt_list)):
+    dp_table[i] = max(dp_table[i-1], dp_table[i-2] + cnt_list[i])
+
+print(dp_table[len(cnt_list) - 1])
