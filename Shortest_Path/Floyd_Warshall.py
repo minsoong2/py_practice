2a6fd2261e1224085893b369ee7721aed045a0be
minsoong2 <happyms817@gmail.com>
2024-10-05 20:16:17 +0900

    Update Floyd Warshall

--- Shortest_Path/Floyd_Warshall.py
+++ Shortest_Path/Floyd_Warshall.py
@@ -1,1 +1,27 @@
-imp
+import sys
+
+inf = int(1e9)
+node, edge = map(int, sys.stdin.readline().split())
+g = [[inf] * (node + 1) for i in range(node + 1)]
+
+for a in range(1, node + 1):
+    for b in range(1, node + 1):
+        if a == b:
+            g[a][b] = 0
+
+for _ in range(edge):
+    a, b, c = map(int, sys.stdin.readline().split())
+    g[a][b] = c
+
+for i in range(1, node + 1):
+    for a in range(1, node + 1):
+        for b in range(1, node + 1):
+            g[a][b] = min(g[a][b], g[a][i] + g[i][b])
+
+for a in range(1, node + 1):
+    for b in range(1, node + 1):
+        if g[a][b] == inf:
+            print("INF")
+        else:
+            print(g[a][b], end= ' ')
+    print()
