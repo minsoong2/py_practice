53116c42011d05c1ec5c32a4494469ff452ba80f
minsoong2 <happyms817@gmail.com>
2024-10-05 22:33:42 +0900

    Update disjoint sets ex

---
+++ Other_Graph_Theory/disjoint_sets_ex.py
@@ -0,0 +1,33 @@
+import sys
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return x
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+node, edge = map(int, sys.stdin.readline().split())
+parent = [0] * (node + 1)
+
+for i in range(1, node + 1):
+    parent[i] = i
+
+for i in range(edge):
+    a, b = map(int, sys.stdin.readline().split())
+    union_parent(parent, a, b)
+
+print('Sets:', end='')
+for i in range(1, node + 1):
+    print(find_parent(parent, i))
+
+print()
+print('Parent table:', end='')
+for i in range(1, node + 1):
+    print(parent[i], end='')
