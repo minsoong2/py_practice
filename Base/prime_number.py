b1b8b36927620cd7960643cab84cf62e9f857e7c
minsoong2 <happyms817@gmail.com>
2024-10-06 14:47:21 +0900

    Update base tips

--- Base/prime_number.py
+++ Base/prime_number.py
@@ -3,3 +3,10 @@ import sympy
 n = 100
 prime_numbers = list(sympy.primerange(0, n))
 print(prime_numbers)  # 0부터 n까지의 소수 목록 출력
+
+def is_prime_number(num):
+    for i in range(2, num):
+        if num % i != 0:
+            return True
+    return False
+
