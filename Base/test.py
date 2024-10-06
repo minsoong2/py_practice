d2fc60244771029d95d50692860c8e58144fe4b3
minsoong2 <happyms817@gmail.com>
2024-10-06 16:46:30 +0900

    Update file name

--- Base/test.py
+++ Base/test.py
@@ -1,17 +1,50 @@
+# import sys
+# a, b, c = map(int, sys.stdin.readline().split())
+# a = int(sys.stdin.readline())
+
+# a, b = map(int, input().split())
+# arr = list(map(int, input().split())) * list 공백기준 선언 *
+# list -> int는 append x
+# print(i, end=' ') end='' 줄바꿈 제거
+# set(list) * set -> list 중복제거 *
+
+# l = [i for i in range(10)] * 파이썬 리스트 정의 *
+
+# 정수 문자열 출력
+# data = list(map(str, input().split()))
+# num = int(data[0])
+# string = data[1]
+
+# k_v = {}
+# for i in range(n):
+#     k, v = map(int, input().split())
+#     k_v[k] = v
+
+# from collections import Counter
+# 출력: Counter({6: 1, 3: 1, 2: 1, 10: 1, -10: 1})
+
+# for element, count in my_counter.items():
+#     print(f"{element}: {count}")
+
 import sys
 
-num, div_num = map(int, sys.stdin.readline().split())
-cnt = 0
+num = int(sys.stdin.readline())
 
-while True:
-    if num % div_num == 0:
-        num = num / div_num
-        cnt += 1
+for i in range(num):
+    string = sys.stdin.readline()
+    cnt_l = 0
+    cnt_r = 0
+    for j in range(len(string)):
+        if cnt_l >= cnt_r:
+            if string[j] == '(':
+                cnt_l += 1
+            elif string[j] == ')':
+                cnt_r += 1
+        else:
+            # ))((
+            break
+
+    if cnt_r == cnt_l:
+        print('YES')
     else:
-        num = num - 1
-        cnt += 1
-
-    if num == 1:
-        break
-
-print(cnt)
+        print('NO')
--- Base/trial_division.py
+++
@@ -1,9 +0,0 @@
-prime_arr = []
-for i in range(2, 100 + 1):
-    cnt = 0
-    for j in range(2, int(i ** 0.5) + 1):
-        if i % j == 0:
-            cnt += 1
-            break
-    if cnt == 0:
-        prime_arr.append(i)
