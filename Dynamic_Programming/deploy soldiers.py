f6581a178bcee2beeba0430b2cce3d589bd0b320
minsoong2 <happyms817@gmail.com>
2024-10-09 15:14:22 +0900

    Update deploy soldiers

--- Dynamic_Programming/deploy soldiers.py
+++ Dynamic_Programming/deploy soldiers.py
@@ -1,13 +1,17 @@
+# 가장 긴 증가하는 부분 수열 (Longest Increasing Subsequence, LIS)
+# dp
+# d[i] = arr[i]를 마지막 원소로 가지는 부분 수열의 최대 길이
+# 모든 0 <= j < i, d[i] = max(d[i], d[j] + 1) if arr[j] <= arr[i]
+
 import sys
 
-num = int(sys.stdin.readline())
-power_list = list(map(int, sys.stdin.readline().split()))
+n = int(sys.stdin.readline())
+n_list = list(map(int, sys.stdin.readline().split()))
+n_list.reverse()
+dp = [1] * n
+for i in range(1, n):
+    for j in range(i):
+        if n_list[i] >= n_list[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
-now = power_list[0]
-cnt = 0
-for i in range(1, len(power_list)):
-    if now < power_list[i]:
-        cnt += 1
-    now = power_list[i]
-
-print(cnt)
+print(dp[n-1])
--- test.py
+++ test.py
@@ -1,16 +1,17 @@
+# 가장 긴 증가하는 부분 수열 (Longest Increasing Subsequence, LIS)
+# dp
+# d[i] = arr[i]를 마지막 원소로 가지는 부분 수열의 최대 길이
+# 모든 0 <= j < i, d[i] = max(d[i], d[j] + 1) if arr[j] <= arr[i]
+
 import sys
 
-n, l = map(int, sys.stdin.readline().split())
-arr = []
+n = int(sys.stdin.readline())
+n_list = list(map(int, sys.stdin.readline().split()))
+n_list.reverse()
+dp = [1] * n
+for i in range(1, n):
+    for j in range(i):
+        if n_list[i] >= n_list[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
-def dfs():
-    if len(arr) == l:
-        re = ' '.join(map(str, arr))
-        print(re)
-        return
-    for i in range(1, n+1):
-        arr.append(i)
-        dfs()
-        arr.pop()
-
-dfs()
+print(dp[n-1])
