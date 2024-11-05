e3cf976622a9e41e0ac60fce3d7e135cad09e5b0
minsoong2 <happyms817@gmail.com>
2024-11-05 22:09:53 +0900

    Update solve maze

--- D,BFS/solve_maze.py
+++ D,BFS/solve_maze.py
@@ -1,33 +1,31 @@
 import sys
-
-m, n = map(int, sys.stdin.readline().split())
-
-g = []
-for l in range(m):
-    g.append(list(map(int, sys.stdin.readline().strip())))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
 from collections import deque
+
 def bfs(x, y):
+
     q = deque()
     q.append((x, y))
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
     while q:
         x, y = q.popleft()
         for i in range(4):
-            move_x = x + dx[i]
-            move_y = y + dy[i]
-            if 0 <= move_x < m and 0 <= move_y < n:
-                if g[move_x][move_y] == 1:
-                    g[move_x][move_y] = g[x][y] + 1
-                    q.append((move_x, move_y))
+            mx = x + dx[i]
+            my = y + dy[i]
+            if 0 <= mx < r and 0 <= my < c:
+                if g[mx][my] == 1:
+                    g[mx][my] = g[x][y] + 1
+                    q.append((mx, my))
                 else:
                     continue
             else:
                 continue
 
-    print(g[m - 1][n - 1])
+r, c = map(int, sys.stdin.readline().split())
+g = []
+for _ in range(r):
+    g.append(list(map(int, sys.stdin.readline().strip())))
 
 bfs(0, 0)
+print(g[r-1][c-1])
