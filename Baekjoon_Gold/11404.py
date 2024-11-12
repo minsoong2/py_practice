157234125fcbc9d1df6635f71d43e8cc08fae205
minsoong2 <happyms817@gmail.com>
2024-11-12 17:59:59 +0900

    solve 11404- Bellman-Ford Algorithm

---
+++ Baekjoon_Gold/11404.py
@@ -0,0 +1,28 @@
+import sys
+
+inf = int(1e9)
+node = int(sys.stdin.readline())
+edge = int(sys.stdin.readline())
+g = [[inf] * (node + 1) for i in range(node + 1)]
+
+for a in range(1, node + 1):
+    for b in range(1, node + 1):
+        if a == b:
+            g[a][b] = 0
+
+for _ in range(edge):
+    a, b, c = map(int, sys.stdin.readline().split())
+    g[a][b] = min(g[a][b], c)
+
+for i in range(1, node + 1):
+    for a in range(1, node + 1):
+        for b in range(1, node + 1):
+            g[a][b] = min(g[a][b], g[a][i] + g[i][b])
+
+for a in range(1, node + 1):
+    for b in range(1, node + 1):
+        if g[a][b] >= inf:
+            print(0, end=' ')
+        else:
+            print(g[a][b], end= ' ')
+    print()
